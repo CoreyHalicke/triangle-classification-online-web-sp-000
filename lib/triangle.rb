1eaacba0141ed95ae51580b1d082a108c2fb8dc9
Corey H <c.l.halicke@gmail.com>
2019-07-29 15:35:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,7 @@ class Triangle
   def kind
     if @one <= 0 || @two <= 0 || @three <= 0
       raise TriangleError
+    elsif 
     elsif @one == @two && @two == @three
       :equilateral
     elsif @two == @three || @one == @three || @one == @two
